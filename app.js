a385a542ff4072498fe252cd11276960f58f755b
feldspar joe <feldsparanyigor@gmail.com>
2025-04-11 01:35:15 +0100

    nnn

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ document.addEventListener('DOMContentLoaded', () =>{
 const buttons = document.querySelectorAll('.btn');
 const billInput = document.getElementById('num');
 const peopleInput = document.getElementById('people');
+const reset = document.querySelector('.reset');
+const customBtn = document.getElementById('custom-btn');
+const customInput = document.getElementById('custom-input');
+
 
 const tipAmountDisplay = document.getElementById('tip-amount');
 const totalAmountDisplay = document.getElementById(('total-amount'));
@@ -10,6 +14,8 @@ const totalAmountDisplay = document.getElementById(('total-amount'));
 let bill = 0;
 let  clickedPercent = 0;
 let totalPeople = 0;
+
+
 
 
 const ClickedButton = (button) => {
@@ -37,12 +43,43 @@ peopleInput.addEventListener('input', () => {
     updateCalculation();
 });
 
+customBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  customBtn.style.display = 'none';
+  customInput.style.display = 'inline-block';
+  customInput.focus();
+});
+
+customInput.addEventListener('input', () => {
+  let customValue = parseFloat(customInput.value);
+  if (!isNaN(customValue)) {
+    clickedPercent = customValue / 100;
+    buttons.forEach(btn => btn.classList.remove('active'));
+    updateCalculation();
+  }
+});
+
+
 function updateCalculation(){
     const tip = (bill * clickedPercent);
     const total = tip * totalPeople;
 
     tipAmountDisplay.textContent = tip.toFixed(2);
     totalAmountDisplay.textContent = total.toFixed(2);
-}
+};
+
+reset.addEventListener('click', () => {
+    billInput.value = '';
+    peopleInput.value = '';
+    tipAmountDisplay.textContent = '0.00';
+    totalAmountDisplay.textContent = '0.00';
+
+    buttons.forEach(button => button.classList.remove('active'));
+    clickedPercent = 0;
+    bill = 0;
+    totalPeople = 0;
+
+    console.log('Reset clicked');
+  });
 
 });
